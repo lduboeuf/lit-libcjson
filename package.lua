3b9f273c1080d9e4748a5d31c80b93707b6a6535
lduboeuf <lionel.duboeuf@marvinbot.com>
2015-05-01 21:27:28 +0200

    initial import

---
+++ package.lua
@@ -0,0 +1,13 @@
+return {
+  name = "dubld/cjson",
+  version = "1.0.0",
+  homepage = "....",
+  dependencies = {
+    "creationix/ffi-loader@1.0.2"
+  },
+  files = {
+    "*.lua",
+    "*.h",
+    "$OS-$ARCH/*",
+  }
+}
